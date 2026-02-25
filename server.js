e6f15fc7725b69049955f5c61defb6912fe9dcbf
bkonai00 <96584691+bkonai00@users.noreply.github.com>
2026-02-25 22:19:46 +0600

    server.js

--- server.js
+++ server.js
@@ -477,6 +477,7 @@ app.get('/stats-overlay/:token', async (req, res) => {
         if (user.overlay_theme === 'minimal') fileToSend = 'overlay_stats_minimal.html';
         if (user.overlay_theme === 'vip') fileToSend = 'overlay_stats_vip.html';
         if (user.overlay_theme === 'basic') fileToSend = 'overlay_stats_basic.html';
+        if (user.overlay_theme === 'frost') fileToSend = 'overlay_stats_frost.html';
     }
     res.sendFile(path.join(__dirname, fileToSend));
 });
@@ -572,6 +573,7 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
 
 
 
